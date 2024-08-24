0dddc3d3ac724a25114e21f5ea6a6035b30a2ac8
harsha <penumalaharshavardhan@gmail.com>
2024-08-24 14:03:16 +0000

    commit messge

---
+++ src/main/java/com/example/todo/service/TodoJpaService.java
@@ -0,0 +1,79 @@
+/*
+ * You can use the following import statements
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.http.HttpStatus;
+ * import org.springframework.stereotype.Service;
+ * import org.springframework.web.server.ResponseStatusException;
+ * import java.util.*;
+ *
+ */
+
+// Write your code here
+package com.example.todo.service;
+
+import com.example.todo.model.Todo;
+import com.example.todo.repository.TodoJpaRepository;
+import com.example.todo.repository.TodoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.*;
+
+@Service
+public class TodoJpaService implements TodoRepository {
+    @Autowired
+    private TodoJpaRepository todoJpaRepository;
+
+    @Override
+    public ArrayList<Todo> getTodos() {
+        List<Todo> todoList = todoJpaRepository.findAll();
+        ArrayList<Todo> todos = new ArrayList<>(todoList);
+        return todos;
+    }
+
+    @Override
+    public Todo getTodoById(int id) {
+        try {
+            Todo todo = todoJpaRepository.findById(id).get();
+            return todo;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public Todo addTodo(Todo todo) {
+        return todoJpaRepository.save(todo);
+    }
+
+    @Override
+    public Todo updateTodo(int id, Todo todo) {
+        try {
+            Todo newTodo = todoJpaRepository.findById(id).get();
+            if (todo.getTodo() != null) {
+                newTodo.setTodo(todo.getTodo());
+            }
+            if (todo.getStatus() != null) {
+                newTodo.setStatus(todo.getStatus());
+            }
+            if (todo.getPriority() != null) {
+                newTodo.setPriority(todo.getPriority());
+            }
+            return newTodo;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public void deleteTodo(int id) {
+        try {
+            todoJpaRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+}
